8f7fc76e4580d7f286e9aac961987e9341bae6a4
Meir Taffel <artgardening@gmail.com>
2017-05-18 15:25:13 +0000

    finish add CRUD for Topic.

--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -1,21 +1,60 @@
 class TopicsController < ApplicationController
+  before_action :get_topic, only: [:show, :edit, :update, :destroy]
+  
   def index
     @topics = current_user.topics.all
   end
 
   def show
-    @topic = Topic.find(params[:id])
   end
 
   def new
-  end
-
-  def edit
+    @topic = Topic.new
   end
   
   def create
+    @topic = current_user.topics.new(topics_params)
+    
+    if @topic.save
+      flash[:notice] = "Topic was saved successfully."
+      redirect_to action: :index
+    else
+      flash.now[:alert] = "Error creating topic. Please try again..."
+      render :new
+    end
+  end
+  
+  def edit
+  end
+  
+  def update
+    @topic.assign_attributes(topics_params)
+    
+    if @topic.save
+      flash[:notice] = "Topic was updated successfully."
+      redirect_to action: :index
+    else
+      flash.now[:alert] = "Error updating topic. Please try again..."
+      render :edit
+    end
   end
   
   def destroy
+    if @topic.destroy
+      flash[:notice] = "\"#{@topic.title}\" was deleted successfully."
+    else
+      flash[:alert] = "There was an error deleting the topic."
+    end
+    redirect_to action: :index
+  end
+  
+  private
+  
+  def topics_params
+    params.require(:topic).permit(:title)
+  end
+  
+  def get_topic
+    @topic = Topic.find(params[:id])
   end
 end
