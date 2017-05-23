062a31746e8d6afe9d2ff85d539e08e6ddb56d5a
Meir Taffel <artgardening@gmail.com>
2017-05-23 11:19:27 +0000

    add bookmark to database

--- app/controllers/incoming_controller.rb
+++ app/controllers/incoming_controller.rb
@@ -1,16 +1,20 @@
 class IncomingController < ApplicationController
   # http://stackoverflow.com/questions/1177863/how-do-i-ignore-the-authenticity-token-for-specific-actions-in-rails
   skip_before_action :verify_authenticity_token, only: [:create]
+  skip_before_action :authenticate_user!
 
   def create
     # Take a look at these in your server logs
     # to get a sense of what you're dealing with.
-    puts "INCOMING PARAMS HERE: #{params}"
+    # puts "INCOMING PARAMS HERE: #{params}"
 
-    # You put the message-splitting and business
-    # magic here.
-
-    # Assuming all went well.
-    head 200
+    user = User.find_by(email: params[:sender])
+    if user
+        topic = user.topics.find_or_create_by(title: params[:subject])
+        bookmark = topic.bookmarks.find_or_create_by(url: params['stripped-text'])
+        head 200
+    else
+        head 401
+    end
   end
 end
